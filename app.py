c892413dcd59106dd786f58aace1f76061433b34
Andrii <96478183+a-sheiko@users.noreply.github.com>
2026-02-09 11:57:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Secure World!'
+def hello():
+    return 'Deployed via Jenkins on AWS EKS.'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
